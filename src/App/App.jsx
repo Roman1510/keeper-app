bcd68fdbcc98becef78501b88cac6fb032c05405
Roman Vinnick <romanvinnick@gmail.com>
2021-01-21 14:23:18 +0600

    initial one

---
+++ src/App/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from '../Header/Header'
+import Note from '../Note/Note'
+import Footer from '../Footer/Footer'
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <Note />
+            <Footer />
+        </div>
+    )
+}
+
+export default App;
